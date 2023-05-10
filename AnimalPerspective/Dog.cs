f456e70b5f73489a3948c4e5d06b24d1f7fc887d
Rafael José <ra.david@hotmail.com>
2023-05-10 18:34:53 +0100

    Exercise 2
    Add interface IMammal and add them to Dog, Cat & Bat

--- AnimalPerspective/Dog.cs
+++ AnimalPerspective/Dog.cs
@@ -2,8 +2,10 @@
 
 namespace AnimalPerspective
 {
-    public class Dog : Animal
+    public class Dog : Animal , IMammal
     {
+        public int NumberOfNipples { get {return 10;} }
+        
         public override string Sound()
         {
             return base.Sound() + "Woof!";
