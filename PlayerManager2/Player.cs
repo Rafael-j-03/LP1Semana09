7837cf412327d7c8efc2ee062f9229d00289cdf2
Rafael José <ra.david@hotmail.com>
2023-05-10 19:19:30 +0100

    Exercise 4
    Copy the methods from PlayerManager1 to PlayerManager2

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager2
+{
+    public class Player
+    {
+        public string Name { get; }
+        public int    Score {get; set;}
+
+        // Constructor
+        public Player(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+    }
+}
